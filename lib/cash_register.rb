f4d01e495018dd06a9637d78299a4da4895396ad
Raúl Reymundi <raul@kickstarter.com>
2022-09-29 22:35:06 -0400

    commit tree 721db0cad0df9be2b2e3aca7bacbcef4a2eac643

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,1 +1,34 @@
 
+class CashRegister
+
+    attr_accessor :discount, :total, :items, :newest_item
+
+    def initialize(discount = 0)
+        @discount = discount
+        @total = 0
+        @items = []
+    end
+
+    def add_item(title, price, quantity = 1)
+        self.newest_item = price * quantity
+        self.total += self.newest_item
+        quantity.times do 
+            self.items << title
+        end
+    end
+
+    def apply_discount
+        if self.discount != 0
+            discount_as_percent = (100.0 - self.discount.to_f) / 100
+            self.total = (self.total * discount_as_percent).to_i
+            "After the discount, the total comes to $#{self.total}."
+          else
+            "There is no discount to apply."
+          end
+    end
+
+    def void_last_transaction
+        self.total -= self.newest_item
+    end
+
+end
